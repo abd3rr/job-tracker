f017e1668588208863fccac2d96863f6a0f4096b
abd3rr <59231440+abd3rr@users.noreply.github.com>
2025-11-19 23:18:39 +0100

    Initial commit: job tracker with applications-service and docker-compose

---
+++ applications-service/src/main/java/com/abd3rr/applications_service/ApplicationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abd3rr.applications_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationsServiceApplication.class, args);
+	}
+
+}
